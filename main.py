7c95e75b5e670282adc50703ee299d483b1ce36d
Albert Zhan <albertzhan@gmail.com>
2016-04-27 10:56:52 -0400

    plz don't touch

--- main.py
+++ main.py
@@ -414,6 +414,19 @@ class Menu():
         self.background = backgroud #background of the menu, will most likely be a rectangle that we stretch (<> -> <==========>)
         self.selected = selected #which item is being selected
         self.items = items #items in the menu (this will most likely be 2d with commands
+    def moveSelect(self):
+        "moves menu selector and returns new value"
+        #moves self.selected up and down
+        kp = key.get_pressed()
+        if kp[K_UP]:
+            self.selected -= 1
+        elif kp[K_DOWN]:
+            self.selected += 1
+        #wrapping around seleced
+        if self.selected < 0:
+            self.selected = len(items) - 1
+        elif self.selected >= len(items):
+            self.selected = 0
     
     
     
